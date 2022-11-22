023994fcb52c0e8a24f62fac720bffa5d753f042
dhanushka92 <54582030+dhanushka92@users.noreply.github.com>
2022-11-22 15:11:11 +0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,11 +6,8 @@ npm install
 
 # 192.168.33.11 and 192.168.33.12 are our two NodeJS backends
 
-ssh vagrant@192.168.33.11 'sudo mkdir -p /app; sudo chown -R vagrant. /app'
-rsync -avz ./ vagrant@192.168.33.11:/app/
-ssh vagrant@192.168.33.11 "sudo systemctl restart nodeapp"
+ssh ec2-user@13.232.39.10 'sudo mkdir -p /app; sudo chown -R ec2-user. /app'
+rsync -avz ./ ec2-user@13.232.39.10:/app/
+ssh ec2-user@13.232.39.10 "sudo systemctl restart mynode"
 
 
-ssh vagrant@192.168.33.12 'sudo mkdir -p /app; sudo chown -R vagrant. /app'
-rsync -avz ./ vagrant@192.168.33.12:/app/
-ssh vagrant@192.168.33.12 "sudo systemctl restart nodeapp"
